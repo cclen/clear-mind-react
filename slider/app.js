157951b4087d2d1f14e87a8ae7a82516958037c2
CC Len <cece.len12@gmail.com>
2021-11-05 08:35:12 -1000

    remove unnecessary comments

--- slider/app.js
+++ slider/app.js
@@ -54,22 +54,7 @@ const applySliderConfigs = function (color, size, range, defaultPosition) {
 const rangeArray1 = [0, 20, 40, 60, 80, 100];
 const rangeArray2 = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-// Tested, Confirmed - Knob doesn't move out of slider min or max on small, medium, large
-
-// Test Knob max position - Confirmed
-// applySliderConfigs("coral", "small", rangeArray2, "max");
-// applySliderConfigs("coral", "medium", rangeArray2, "max");
-// applySliderConfigs("coral", "large", rangeArray2, "max");
-
-// Test Knob min position - Confirmed
-// applySliderConfigs("aqua", "small", rangeArray2, "min");
-// applySliderConfigs("aqua", "medium", rangeArray2, "min");
-applySliderConfigs("aqua", "large", rangeArray2, "min");
-
-// Test Knob middle position - Confirmed
-// applySliderConfigs("lime", "small", rangeArray2, "middle");
-// applySliderConfigs("lime", "medium", rangeArray2, "middle");
-// applySliderConfigs("lime", "large", rangeArray2, "middle");
+applySliderConfigs("aqua", "medium", rangeArray2, "middle");
 
 const grabKnob = function(event) {
     isDown = true;
@@ -100,7 +85,6 @@ const moveKnob = function(event) {
         } 
     }
 }
-
 
 sliderKnob.addEventListener("mousedown", grabKnob);
 sliderKnob.addEventListener("mouseup", releaseKnob);
