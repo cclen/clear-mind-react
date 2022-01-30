802ca3708d13d8ce03d31b7e0395fd8cc1ca67a7
CC <cecilia.lengyel@gmail.com>
2022-01-30 12:12:38 -1000

    initial build of routers, slider

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 import Slider from "./components/Slider";
 import Store from "./components/Store";
 import Files from "./components/Files";
+import FancyButton from "./components/FancyButton";
 
 
 function App() {
@@ -18,12 +19,14 @@ function App() {
           <Link to="/slider">Slider</Link>
           <Link to="/store">Store</Link>
           <Link to="/files">Files</Link>
+          <Link to="/fancybutton">FancyButton</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/slider" element={<Slider />}></Route>
           <Route path="/store" element={<Store />}></Route>
           <Route path="/files" element={<Files />}></Route>
+          <Route path="/fancybutton" element={<FancyButton />}></Route>
         </Routes>
       </Router>
     </div>
--- react-app/src/components/Slider.js
+++ react-app/src/components/Slider.js
@@ -1,40 +1,90 @@
 import React from "react";
-
 import "./Slider.css";
-
-const sliderConfigs = {
-    knobColor: "#d5d5d5",
-    sliderSize: "medium",
-    sliderRange: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-    knobPosition: "middle"
-}
+import DocumentEvents from 'react-document-events';
 
 class Slider extends React.Component {
-    render() {
-        const range = sliderConfigs.sliderRange;
+    constructor(props) {
+        super(props);
+        this.state = {
+            sliderRange: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+            isDown: false,
+            offset: null,
+            knobTopPosition: "-28px",
+            containerHeight: "400px"
+        };
+    }
 
-        const labelsList = range.map((label)=>{
-            return <li>{label} &mdash; </li>;
-        });
+    grabKnob(event) {
+        this.setState(prevState => ({ 
+            isDown: true,
+            offset: event.target.offsetTop - event.clientY
+        }));
+    }
+
+    releaseKnob(event) {
+        this.setState(prevState => ({ 
+            isDown: false
+        }));
+    }
+
+    moveKnob(event) {
+        event.preventDefault();
+        let knobOffset;
+        let containerHeight = this.state.containerHeight;
+        
+        if (this.state.isDown) {
+            knobOffset = (event.clientY + this.state.offset);
+        
+            if (knobOffset < -28) {
+                this.setState(prevState => ({ 
+                    knobTopPosition: "-28px"
+                }));
+            } else if (
+                (containerHeight === "300px" && knobOffset > 239) || 
+                (containerHeight === "400px" && knobOffset > 350) || 
+                (containerHeight === "500px" && knobOffset > 439)) {
+                this.setState(prevState => ({ 
+                    knobTopPosition: "calc(100% - 100px)"
+                }));
+            } else {
+                this.setState(prevState => ({ 
+                    knobTopPosition: knobOffset + 'px'
+                }));
+            }
+        }
+    }
+    
+    render() {
+        const range = this.state.sliderRange;
+
+        const labelsList = range.map( label => <li key={label}>{label} &mdash; </li>);
 
         return(
             <div>
                 <h1>Slider</h1>
-                <div class="sliderContainer">
-                    <div class="labelsContainer">
-                        <ul class="labelsList">
+                <div className="sliderContainer">
+                    <div className="labelsContainer">
+                        <ul className="labelsList">
                             {labelsList}
                         </ul>
-                        <div class="slider-track"></div>
+                        <div className="sliderTrack"></div>
                     </div>
-                    <div id="slider-knob">
-                        <div class="horizontal-line"></div>
+                    
+                    <div
+                        className="sliderKnob"
+                        style={{top: this.state.knobTopPosition}}
+                        onMouseDown={e => this.grabKnob(e) }
+                    >
+                        <div className="horizontalLine"></div>
                     </div>
                 </div>
+                <DocumentEvents 
+                    onMouseUp={e => this.releaseKnob(e) }
+                    onMouseMove={e => this.moveKnob(e) }
+                />
             </div>
         );
     }
-
 }
 
 export default Slider;
